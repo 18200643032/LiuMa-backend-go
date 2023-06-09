321f4be25bd451ee5f0ca37b3449bc215fec441a
emoob <379946303@qq.com>
2023-06-09 16:57:02 +0800

    add

--- internal/api/middleware/auth.go
+++ internal/api/middleware/auth.go
@@ -3,18 +3,15 @@ package middleware
 import (
 	"LiuMa-backend-go/internal/api/e"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.Request.Header.Get("Authorization")
 		if token == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": e.MsgFlags(e.ERROR_AUTH)})
-			c.Abort()
+			e.ErrorMsg(c, e.StatusUnauthorized, e.MsgFlags(e.ERROR_AUTH))
 			return
 		}
-
 		c.Next()
 	}
 }
