f7f47b90f8d5cb8fc09e8c7d4bbdf49667ae8d30
zhengzhong <910386943@qq.com>
2024-09-05 11:29:19 +0800

    v1

---
+++ apps/user/admin/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"LiuMa-backend-go/apps/user/admin/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
